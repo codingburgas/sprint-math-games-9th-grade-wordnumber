bc19e6eb607fa9cd8fd1a4202063f13ac987c853
SDTanev24 <SDTanev24@codingburgas.bg>
2025-12-03 21:36:32 +0200

    Design the chooseGame menu

--- WordNumber/ConsoleApplication1/chooseGame.cpp
+++ WordNumber/ConsoleApplication1/chooseGame.cpp
@@ -5,10 +5,10 @@ void chooseGame() {
     const int screenWidth = 1920;
     const int screenHeight = 1080;
 
-    const Rectangle quiz1Button = { screenWidth / 2 - 450, screenHeight / 2 - 330, 300, 120 };
-    const Rectangle quiz2Button = { screenWidth / 2 + 250, screenHeight / 2 - 330, 300, 120 };
-    const Rectangle quiz3Button = { screenWidth / 2 - 450, screenHeight / 2 - 110, 300, 120 };
-    const Rectangle quiz4Button = { screenWidth / 2 + 250, screenHeight / 2 - 110, 300, 120 };
+    const Rectangle game1Button = { screenWidth / 2 - 450, screenHeight / 2 - 160, 300, 120 };
+    const Rectangle game2Button = { screenWidth / 2 + 250, screenHeight / 2 - 160, 300, 120 };
+    const Rectangle game3Button = { screenWidth / 2 - 450, screenHeight / 2 - 10, 300, 120 };
+    const Rectangle game4Button = { screenWidth / 2 + 250, screenHeight / 2 - 10, 300, 120 };
     const Rectangle backButton = { screenWidth / 2 + 475, screenHeight / 2 + 280, 300, 120 };
 
     Texture2D background = LoadTexture("../images/background1.png");
@@ -20,31 +20,31 @@ void chooseGame() {
         BeginDrawing();
         ClearBackground(WHITE);
         DrawTexture(background, 0, 0, WHITE);
-        DrawText("Welcome to the games menu! From here you can choose what game to play!", GetScreenWidth() / 2 - 800, GetScreenHeight() / 2 - 455, 50, BLACK);
-        bool isMouseOverQuiz1 = CheckCollisionPointRec(mousePosition, quiz1Button);
-        DrawRectangleRec(quiz1Button, isMouseOverQuiz1 ? DARKGRAY : SKYBLUE);
-        DrawText("Hangman", screenWidth / 2 - 385, screenHeight / 2 - 290, 50, WHITE);
+        DrawText("Welcome to the games menu! From here you can choose what game to play!", GetScreenWidth() / 2 - 875, GetScreenHeight() / 2 - 455, 47, WHITE);
+        bool isMouseOverQuiz1 = CheckCollisionPointRec(mousePosition, game1Button);
+        DrawRectangleRec(game1Button, isMouseOverQuiz1 ? DARKGRAY : SKYBLUE);
+        DrawText("Hangman", screenWidth / 2 - 403, screenHeight / 2 - 120, 50, WHITE);
         if (isMouseOverQuiz1 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             hangman();
         }
 
-        bool isMouseOverQuiz2 = CheckCollisionPointRec(mousePosition, quiz2Button);
-        DrawRectangleRec(quiz2Button, isMouseOverQuiz2 ? DARKGRAY : SKYBLUE);
-        DrawText("Math Game", screenWidth / 2 + 315, screenHeight / 2 - 290, 50, WHITE);
+        bool isMouseOverQuiz2 = CheckCollisionPointRec(mousePosition, game2Button);
+        DrawRectangleRec(game2Button, isMouseOverQuiz2 ? DARKGRAY : SKYBLUE);
+        DrawText("Math Game", screenWidth / 2 + 275, screenHeight / 2 - 120, 50, WHITE);
         if (isMouseOverQuiz2 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             startMathGame();
         }
 
-        bool isMouseOverQuiz3 = CheckCollisionPointRec(mousePosition, quiz3Button);
-        DrawRectangleRec(quiz3Button, isMouseOverQuiz3 ? DARKGRAY : SKYBLUE);
-        DrawText("Game 3", screenWidth / 2 - 385, screenHeight / 2 - 70, 50, WHITE);
+        bool isMouseOverQuiz3 = CheckCollisionPointRec(mousePosition, game3Button);
+        DrawRectangleRec(game3Button, isMouseOverQuiz3 ? DARKGRAY : SKYBLUE);
+        DrawText("Game 3", screenWidth / 2 - 385, screenHeight / 2 + 30, 50, WHITE);
         if (isMouseOverQuiz3 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             ;
         }
 
-        bool isMouseOverQuiz4 = CheckCollisionPointRec(mousePosition, quiz4Button);
-        DrawRectangleRec(quiz4Button, isMouseOverQuiz4 ? DARKGRAY : SKYBLUE);
-        DrawText("Game 4", screenWidth / 2 + 315, screenHeight / 2 - 70, 50, WHITE);
+        bool isMouseOverQuiz4 = CheckCollisionPointRec(mousePosition, game4Button);
+        DrawRectangleRec(game4Button, isMouseOverQuiz4 ? DARKGRAY : SKYBLUE);
+        DrawText("Game 4", screenWidth / 2 + 315, screenHeight / 2 +30, 50, WHITE);
         if (isMouseOverQuiz4 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             ;
         }
