60c915f11d437ef4ae4b5c6900df4f1f6bf4f6dc
VIIvanov24 <VIIvanov24@codingburgas.bg>
2025-12-07 22:13:21 +0200

    Update overall design

--- WordNumber/ConsoleApplication1/menu.cpp
+++ WordNumber/ConsoleApplication1/menu.cpp
@@ -1,59 +1,70 @@
-#include "menu.h" 
-#include "chooseGame.h" 
+#include "menu.h"
+#include "chooseGame.h"
 #include "rules.h"
 
 void menu(bool hasInit) {
-	const int screenWidth = 1920; 
-	const int screenHeight = 1080; 
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-	if (!hasInit)
-	{
-		InitWindow(screenWidth, screenHeight, "WordNumber");
-	}
-	Image logoIcon = LoadImage("../images/logo.png");
-	SetWindowIcon(logoIcon);
-	Vector2 mousePosition = GetMousePosition();
-	Texture2D logo = LoadTexture("../images/logo.png");
-	Texture2D menuBg = LoadTexture("../images/menuBackground.png");
-	const Rectangle startQuizButton = { (screenWidth / 2)-110, (screenHeight / 2) - 60, 270, 100 };
-	const Rectangle rulesButton = { (screenWidth / 2)-110, (screenHeight / 2) + 110, 270, 100 };
-	const Rectangle exitButton = { (screenWidth / 2)-110, (screenHeight / 2) + 280, 270, 100 };
+    if (!hasInit) {
+        InitWindow(screenWidth, screenHeight, "WordNumber");
+    }
 
-	while (!WindowShouldClose()) {
+    Image logoIcon = LoadImage("../images/logo.png");
+    SetWindowIcon(logoIcon);
 
-		mousePosition = GetMousePosition();
+    Texture2D logo = LoadTexture("../images/logo.png");
+    Texture2D menuBg = LoadTexture("../images/menuBackground.png");
 
-		BeginDrawing();
-		DrawTexture(menuBg, 0, 0, RAYWHITE);
-		DrawTexture(logo, (screenWidth / 2) - 850, (screenHeight / 2) - 440,RAYWHITE);
-		bool isMouseOverStart = CheckCollisionPointRec(mousePosition, startQuizButton);
-		DrawRectangleRec(startQuizButton, (isMouseOverStart ? SKYBLUE : BLUE));
-		DrawText("Start", screenWidth / 2 -50, screenHeight / 2 - 30, 50, BLACK);
+    Rectangle startQuizButton = { (screenWidth / 2) - 200, (screenHeight / 2) - 40, 400, 100 };
+    Rectangle rulesButton = { (screenWidth / 2) - 200, (screenHeight / 2) + 100, 400, 100 };
+    Rectangle exitButton = { (screenWidth / 2) - 200, (screenHeight / 2) + 240, 400, 100 };
 
-		if (isMouseOverStart && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-			chooseGame();
-		}
+    Color btnStart = Color{ 60, 180, 255, 230 };
+    Color btnRules = Color{ 120, 255, 120, 230 };
+    Color btnExit = Color{ 255, 80, 80, 230 };
 
-		bool isMouseOverRules = CheckCollisionPointRec(mousePosition, rulesButton);
-		DrawRectangleRec(rulesButton, (isMouseOverRules ? GREEN : LIME));
-		DrawText("Rules", screenWidth / 2 -50, screenHeight / 2 +135, 50, BLACK);
+    Color btnStartHover = Color{ 90, 200, 255, 255 };
+    Color btnRulesHover = Color{ 150, 255, 150, 255 };
+    Color btnExitHover = Color{ 255, 120, 120, 255 };
 
-		if (isMouseOverRules && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-			rules();
-		}
+    while (!WindowShouldClose()) {
+        Vector2 mousePosition = GetMousePosition();
 
+        BeginDrawing();
+        ClearBackground(BLACK);
 
-		bool isMouseOverExit = CheckCollisionPointRec(mousePosition, exitButton);
-		DrawRectangleRec(exitButton, (isMouseOverExit ? PINK : RED));
-		DrawText("Exit", screenWidth / 2-25, screenHeight / 2 + 305, 50, BLACK);
+        DrawTexture(menuBg, 0, 0, RAYWHITE);
 
-		if (isMouseOverExit && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-			CloseWindow();
-		}
+        DrawTexture(logo, (screenWidth / 2) - logo.width / 2, 80, RAYWHITE);
 
-		ClearBackground(DARKBLUE);
+        auto drawButton = [&](Rectangle rect, const char* text, Color base, Color hover) {
+            bool isHover = CheckCollisionPointRec(mousePosition, rect);
+            Color finalColor = isHover ? hover : base;
 
-		EndDrawing();
-	}
+            DrawRectangleRounded(rect, 0.3f, 12, Fade(finalColor, 0.92f));
+            DrawRectangleRoundedLines(rect, 0.3f, 12, 3, Fade(WHITE, 0.85f));
+
+            int textWidth = MeasureText(text, 50);
+            DrawText(text, rect.x + (rect.width - textWidth) / 2, rect.y + 28, 50, BLACK);
+
+            return isHover;
+            };
+
+        bool hStart = drawButton(startQuizButton, "Start", btnStart, btnStartHover);
+        bool hRules = drawButton(rulesButton, "Rules", btnRules, btnRulesHover);
+        bool hExit = drawButton(exitButton, "Exit", btnExit, btnExitHover);
+
+        if (hStart && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            chooseGame();
+        }
+        if (hRules && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            rules();
+        }
+        if (hExit && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            CloseWindow();
+        }
+
+        EndDrawing();
+    }
 }
-
--- WordNumber/ConsoleApplication1/rules.cpp
+++ WordNumber/ConsoleApplication1/rules.cpp
@@ -2,6 +2,7 @@
 #include "menu.h"
 #include "raylib.h"
 #include <cmath>
+
 void DrawRoundedRectangle(Rectangle rec, float radius, Color color)
 {
     if (radius <= 0.0f)
@@ -12,14 +13,16 @@ void DrawRoundedRectangle(Rectangle rec, float radius, Color color)
 
     if (radius > rec.width / 2.0f) radius = rec.width / 2.0f;
     if (radius > rec.height / 2.0f) radius = rec.height / 2.0f;
+
     Rectangle centerH{ rec.x + radius, rec.y, rec.width - 2.0f * radius, rec.height };
     Rectangle centerV{ rec.x, rec.y + radius, rec.width, rec.height - 2.0f * radius };
 
     DrawRectangleRec(centerH, color);
     DrawRectangleRec(centerV, color);
+
     Vector2 c1{ rec.x + radius,             rec.y + radius };
     Vector2 c2{ rec.x + rec.width - radius, rec.y + radius };
-    Vector2 c3{ rec.x + radius,            rec.y + rec.height - radius };
+    Vector2 c3{ rec.x + radius,             rec.y + rec.height - radius };
     Vector2 c4{ rec.x + rec.width - radius, rec.y + rec.height - radius };
 
     DrawCircleV(c1, radius, color);
@@ -27,6 +30,7 @@ void DrawRoundedRectangle(Rectangle rec, float radius, Color color)
     DrawCircleV(c3, radius, color);
     DrawCircleV(c4, radius, color);
 }
+
 void DrawRoundedRectangleLines(Rectangle rec, float radius, float thick, Color color)
 {
     if (thick <= 0.0f) return;
@@ -53,6 +57,7 @@ void DrawRoundedRectangleLines(Rectangle rec, float radius, float thick, Color c
 
         float rad = radius - (float)i;
         if (rad < 0.0f) rad = 0.0f;
+
         DrawLine(
             (int)(r.x + rad), (int)r.y,
             (int)(r.x + r.width - rad), (int)r.y,
@@ -74,10 +79,10 @@ void DrawRoundedRectangleLines(Rectangle rec, float radius, float thick, Color c
             color
         );
 
-        Vector2 c1{ r.x + rad,             r.y + rad };
-        Vector2 c2{ r.x + r.width - rad,   r.y + rad };
-        Vector2 c3{ r.x + rad,             r.y + r.height - rad };
-        Vector2 c4{ r.x + r.width - rad,   r.y + r.height - rad };
+        Vector2 c1{ r.x + rad,           r.y + rad };
+        Vector2 c2{ r.x + r.width - rad, r.y + rad };
+        Vector2 c3{ r.x + rad,           r.y + r.height - rad };
+        Vector2 c4{ r.x + r.width - rad, r.y + r.height - rad };
 
         DrawCircleLines((int)c1.x, (int)c1.y, rad, color);
         DrawCircleLines((int)c2.x, (int)c2.y, rad, color);
@@ -85,23 +90,32 @@ void DrawRoundedRectangleLines(Rectangle rec, float radius, float thick, Color c
         DrawCircleLines((int)c4.x, (int)c4.y, rad, color);
     }
 }
+
 void rules()
 {
     const int screenW = GetScreenWidth();
     const int screenH = GetScreenHeight();
 
     Rectangle panel{
-        screenW / 2.0f - 400.0f,
-        screenH / 2.0f - 250.0f,
-        800.0f,
-        500.0f
+        screenW / 2.0f - 500.0f,
+        screenH / 2.0f - 280.0f,
+        1000.0f,
+        560.0f
     };
 
     Rectangle toGame{
-        panel.x + panel.width - 210.0f,
-        panel.y + panel.height - 70.0f,
-        180.0f,
-        50.0f
+        panel.x + panel.width - 230.0f,
+        panel.y + panel.height - 80.0f,
+        200.0f,
+        55.0f
+    };
+
+    const char* ruleLines[5] = {
+        "Welcome to our space quiz adventure!",
+        "Travel through the system and answer questions.",
+        "Use the mouse to choose the correct answer.",
+        "Press ENTER to move to the next challenge.",
+        "Try to finish with the highest score!"
     };
 
     while (!WindowShouldClose())
@@ -114,34 +128,52 @@ void rules()
 
         BeginDrawing();
         ClearBackground(BLACK);
+
         DrawRectangleGradientV(
             0, 0, screenW, screenH,
-            Color{ 10, 10, 30, 255 },
-            Color{ 5, 0, 40, 255 }
+            Color{ 5, 5, 25, 255 },
+            Color{ 2, 0, 40, 255 }
         );
 
-        float planetY = screenH / 2.0f + 30.0f + sinf(t * 0.7f) * 10.0f;
+        for (int i = 0; i < 90; i++)
+        {
+            int sx = (i * 71 + (int)(t * 40)) % screenW;
+            int sy = (i * 47 + (int)(t * 30)) % screenH;
+            Color starCol = (i % 9 == 0) ? Color{ 120, 200, 255, 180 } : Color{ 180, 180, 220, 120 };
+            DrawPixel(sx, sy, starCol);
+        }
+
+        float planetY = screenH / 2.0f + 40.0f + sinf(t * 0.7f) * 12.0f;
         DrawCircleGradient(
-            (int)(panel.x - 80.0f),
+            (int)(panel.x - 90.0f),
             (int)planetY,
-            160.0f,
+            170.0f,
             Color{ 50, 200, 255, 255 },
             Color{ 10, 30, 80, 255 }
         );
         DrawCircleLines(
-            (int)(panel.x - 80.0f),
+            (int)(panel.x - 90.0f),
             (int)planetY,
-            170.0f,
+            182.0f,
             Color{ 0, 255, 180, 120 }
         );
 
-        Color neon = Color{ 0, 255, 200, 255 };
-        Color panelFill = Color{ 10, 10, 25, 230 };
+        Rectangle panelShadow{
+            panel.x + 8.0f,
+            panel.y + 10.0f,
+            panel.width,
+            panel.height
+        };
+        DrawRoundedRectangle(panelShadow, 26.0f, Color{ 0, 0, 0, 120 });
 
-        DrawRoundedRectangle(panel, 24.0f, panelFill);
-        DrawRoundedRectangleLines(panel, 24.0f, 3.0f, neon);
+        Color neon = Color{ 0, 255, 200, 255 };
+        Color panelFill = Color{ 10, 10, 28, 235 };
+
+        DrawRoundedRectangle(panel, 26.0f, panelFill);
+        DrawRoundedRectangleLines(panel, 26.0f, 3.0f, neon);
+
         const char* title = "WordNumber RULES";
-        int titleFontSize = 40;
+        int titleFontSize = 42;
         int titleWidth = MeasureText(title, titleFontSize);
         DrawText(
             title,
@@ -150,27 +182,61 @@ void rules()
             titleFontSize,
             neon
         );
-        int textSize = 22;
-        int textX = (int)panel.x + 60;
-        int textY = (int)panel.y + 90;
-        int lineGap = 35;
 
-        DrawText("Welcome to our space quiz!", textX, textY, textSize, RAYWHITE);
-        DrawText("Travel through the solar system and", textX, textY + lineGap, textSize, RAYWHITE);
-        DrawText("answer questions to earn points.", textX, textY + 2 * lineGap, textSize, RAYWHITE);
-        DrawText("Use the mouse to choose an answer,", textX, textY + 3 * lineGap, textSize, RAYWHITE);
-        DrawText("then press ENTER to go to the next.", textX, textY + 4 * lineGap, textSize, RAYWHITE);
-        DrawText("Try to finish with the highest score!", textX, textY + 5 * lineGap, textSize, RAYWHITE);
+        int subtitleFont = 22;
+        const char* subtitle = "Learn the rules using these flashcards:";
+        int subtitleWidth = MeasureText(subtitle, subtitleFont);
+        DrawText(
+            subtitle,
+            (int)(panel.x + (panel.width - (float)subtitleWidth) / 2.0f),
+            (int)(panel.y + 70),
+            subtitleFont,
+            Color{ 180, 220, 255, 255 }
+        );
 
-        for (int i = 0; i < 5; i++)
+        int cardCount = 5;
+        float cardWidth = panel.width - 120.0f;
+        float cardHeight = 70.0f;
+        float startY = panel.y + 110.0f;
+        float cardGap = 12.0f;
+
+        for (int i = 0; i < cardCount; i++)
         {
-            DrawCircle(textX - 20, textY + i * lineGap + 10, 5, neon);
-        }
-        Color startFill = hoverStart ? Color{ 0, 200, 160, 255 } : Color{ 0, 140, 120, 255 };
-        Color startBorder = Color{ 0, 255, 200, 255 };
+            float cx = panel.x + 60.0f;
+            float cy = startY + i * (cardHeight + cardGap);
 
-        DrawRoundedRectangle(toGame, 18.0f, startFill);
-        DrawRoundedRectangleLines(toGame, 18.0f, 2.0f, startBorder);
+            Rectangle cardRec{
+                cx,
+                cy,
+                cardWidth,
+                cardHeight
+            };
+
+            Color cardFill = Color{ 18, 20, 45, 235 };
+            Color cardBorder = Color{ 70, 200, 255, 220 };
+
+            DrawRoundedRectangle(cardRec, 16.0f, cardFill);
+            DrawRoundedRectangleLines(cardRec, 16.0f, 2.0f, cardBorder);
+
+            Vector2 bulletPos{
+                cardRec.x + 26.0f,
+                cardRec.y + cardRec.height / 2.0f
+            };
+            DrawCircleV(bulletPos, 6.0f, neon);
+            DrawCircleLines((int)bulletPos.x, (int)bulletPos.y, 8.0f, Color{ 0, 255, 200, 120 });
+
+            int ruleFontSize = 22;
+            int textX = (int)(cardRec.x + 50.0f);
+            int textY = (int)(cardRec.y + (cardRec.height - ruleFontSize) / 2.0f);
+
+            DrawText(ruleLines[i], textX, textY, ruleFontSize, RAYWHITE);
+        }
+
+        Color startFill = hoverStart ? Color{ 0, 210, 170, 255 } : Color{ 0, 150, 130, 255 };
+        Color startBorder = Color{ 0, 255, 220, 255 };
+
+        DrawRoundedRectangle(toGame, 20.0f, startFill);
+        DrawRoundedRectangleLines(toGame, 20.0f, 2.0f, startBorder);
 
         const char* startText = "CHOOSE GAME";
         int startFontSize = 24;
@@ -178,18 +244,24 @@ void rules()
         DrawText(
             startText,
             (int)(toGame.x + (toGame.width - (float)startTextWidth) / 2.0f),
-            (int)(toGame.y + 10),
+            (int)(toGame.y + 14),
             startFontSize,
             RAYWHITE
         );
 
-        DrawText("Press ESC to go back",
-            (int)(panel.x + 20),
+        const char* escText = "Press ESC to go back to the menu";
+        int escFont = 18;
+        int escW = MeasureText(escText, escFont);
+        DrawText(
+            escText,
+            (int)(panel.x + (panel.width - escW) / 2.0f),
             (int)(panel.y + panel.height - 40),
-            18,
-            GRAY);
+            escFont,
+            GRAY
+        );
 
         EndDrawing();
+
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hoverStart)
             chooseGame();
     }
